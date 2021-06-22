75f1655cd64feac4253cd00270e1df61e9211475
jpeysakhova17 <81653295+jpeysakhova17@users.noreply.github.com>
2021-06-21 20:04:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+//number1
+
+let Jane = {
+        name: "Jane",
+        score: 95,
+        date: "2020-01-24",
+        passed: true
+}
+
+let Joe = {
+        name: "Joe",
+        score: 77,
+        date: "2018-05-14",
+        passed: true
+    }
+    
+let Jack = {
+        name: "Jack",
+        score: 59,
+        date: "2019-07-05",
+        passed: false
+    }
+
+let Jill = {
+        name: "Jill",
+        score: 88,
+        date: "2020-04-22",
+        passed: true
+    }
+
+const submissions = [Jane, Joe, Jack, Jill];
+
+//number 2
+
+function addSubmission (array, newName, newScore, newDate) {
+    submissions.push(Jake);
+}
+
+let Jake = {
+    name: "Jake",
+    score: 35,
+    date: "2020-04-22",
+    passed: score >= 60 ? true: false,
+}
+
+//Declare a function named addSubmission
+//Parameter(s): array, newName, newScore, newDate
+//Functionality: construct an object and push it into the array. 
+//The object must have the same properties as the objects already in the array. 
+//Use conditional statements to set the value for the passed property to true if 
+//the score is greater than or equal to 60 and false otherwise.
+
